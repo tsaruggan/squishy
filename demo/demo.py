4926189ad2fa5fc3d305906c7f496f6ec07c655c
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2022-01-01 13:23:02 -0500

    changed demo to use PNGs not JPGs

--- demo/demo.py
+++ demo/demo.py
@@ -9,11 +9,11 @@ from squishy import *
 if __name__ == '__main__':
     start = time.time()
     squishy = Squishy()
-    squishy.compress('octopus.jpg')
+    squishy.compress('octopus.png')
     print('-' * 40)
     squishy.decompress('octopus.bin', 'octopus_new.png')
     stop = time.time()
     times = (stop - start) * 1000
     print('-' * 40)
     print('Run time takes %d miliseconds' % times)
-    print('Images equal = %s' % images_equal('octopus.jpg', 'octopus_new.png'))
+    print('Images equal = %s' % images_equal('octopus.png', 'octopus_new.png'))
